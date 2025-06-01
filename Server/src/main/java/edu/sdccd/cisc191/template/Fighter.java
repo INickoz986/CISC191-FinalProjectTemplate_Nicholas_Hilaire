b0d2f3e6bf60eeecfc18e3d1357a62bba956f1c4
Rabit-E <verypro207@gmail.com>
2025-05-31 19:12:47 -0700

    Update Fighter.java

--- Server/src/main/java/edu/sdccd/cisc191/template/Fighter.java
+++ Server/src/main/java/edu/sdccd/cisc191/template/Fighter.java
@@ -3,40 +3,35 @@ package edu.sdccd.cisc191.template;
 /**
  * Author Nicholas Hilaire
  *
- *
  * References: "Bro code: Java: Inheritance" https://www.youtube.com/watch?v=Zs342ePFvRI
  */
 
-
-// Fighter class inherits the methods of Unit Class.
+// Fighter unit extends Unit
 public class Fighter extends Unit
 {
+    //TODO: stores fuel value
     private int fuel;
 
-    //Constructor to Initialize the private objects in infantryUnit and Unit class.
+    // sets everything up, including what's in Unit
     public Fighter(String unitName, String unitType, String specialization, int price, int armor,
                    int health, int sightRange, double unseenRange, int speed,
                    int weight, String abilities, int fuel)
     {
-        // Inheretence so InfantryUnit Class can inherent and actually utilize the methods from the Unit super class.
-        super (unitName, unitType, specialization, price, armor, health, sightRange, unseenRange,speed, weight, abilities);
-
-        //Setters created to set the value of the variable in the InfantryUnit Class
+        //TODO: call parent constructor first
+        super(unitName, unitType, specialization, price, armor, health, sightRange, unseenRange, speed, weight, abilities);
         this.fuel = fuel;
     }
 
-        // Returns the value that is in the CSV file as extra stat.
-        public int getFuel()
-        {
-            return fuel;
-        }
+    //TODO: just returns fuel value
+    public int getFuel()
+    {
+        return fuel;
+    }
 
-            // Method created to change ExtraStat title in CSV to fuel and return that corresponding value
-            @Override
-            public String toString()
-            {
-                return super.toString() + "fuel= " + fuel;
-            }
-
-
+    // includes fuel in the output
+    @Override
+    public String toString()
+    {
+        return super.toString() + "fuel= " + fuel;
+    }
 }
