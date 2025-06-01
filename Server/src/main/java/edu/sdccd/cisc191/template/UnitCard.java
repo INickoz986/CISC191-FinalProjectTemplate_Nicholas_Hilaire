f8f546fe883706c653a4f8bd80e2051e4cc7e334
Rabit-E <verypro207@gmail.com>
2025-05-31 19:13:34 -0700

    Update UnitCard.java

--- Server/src/main/java/edu/sdccd/cisc191/template/UnitCard.java
+++ Server/src/main/java/edu/sdccd/cisc191/template/UnitCard.java
@@ -2,23 +2,23 @@ package edu.sdccd.cisc191.template;
 
 import javafx.geometry.Insets;
 import javafx.scene.control.Label;
-import javafx.scene.layout.VBox;
-import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
 
-
 /**
  * Author Nicholas Hilaire
  *
- * "Java: CSV File Easy Read/Write" https://stackoverflow.com/questions/14226830/java-csv-file-easy-read-write
- * "How to Use BoxLayout"  https://docs.oracle.com/javase/tutorial/uiswing/layout/box.html
+ * "Java: CSV File Easy Read/Write" https://stackoverflow.com/questions/14226830/java-csv-file-easy-read-write  
+ * "How to Use BoxLayout" https://docs.oracle.com/javase/tutorial/uiswing/layout/box.html
  */
 
 public class UnitCard extends VBox
 {
+    //TODO: image of the unit
     private ImageView unitImageView;
+
+    //TODO: labels for unit info
     private Label unitNameLabel;
     private Label unitTypeLabel;
     private Label specializationLabel;
@@ -27,19 +27,20 @@ public class UnitCard extends VBox
 
     public UnitCard()
     {
-        super(5); // spacing between each specific unit stat
-        // Set padding and border styling
+        super(5); // spacing between each field
+
+        //TODO: styling and padding
         setPadding(new Insets(10));
         setStyle("-fx-border-color: black; -fx-background-color: #f0f0f0;");
 
-        //Initialize Imageview so Unit stats can have a photo.
+        //TODO: set up image view
         unitImageView = new ImageView();
-        unitImageView.setId("unitImageView"); // allows lookup by id
+        unitImageView.setId("unitImageView");
         unitImageView.setFitWidth(120);
         unitImageView.setFitHeight(80);
         unitImageView.setPreserveRatio(true);
 
-        // Initialize labels so it will display on the application.
+        //TODO: set up labels
         unitNameLabel = new Label();
         unitNameLabel.setStyle("-fx-font-size: 16px; -fx-font-weight: bold;");
         unitTypeLabel = new Label();
@@ -47,22 +48,21 @@ public class UnitCard extends VBox
         statsLabel = new Label();
         abilitiesLabel = new Label();
 
-        // Add labels to the VBox so it will display on application.
+        //TODO: add everything to the VBox
         getChildren().addAll(unitNameLabel, unitTypeLabel, specializationLabel, statsLabel, abilitiesLabel);
     }
 
-        // Generates the Strings to label the data types from Unit class to display on applicaiton.
-        public void setUnit(Unit unit)
-        {
-            unitNameLabel.setText(unit.getUnitName());
-            unitTypeLabel.setText("Type: " + unit.getUnitType());
-            specializationLabel.setText("Spec: " + unit.getSpecialization());
-            statsLabel.setText("Price: " + unit.getPrice() + " | Armor: " + unit.getArmor() +
-                    " | Health: " + unit.getHealth());
-            abilitiesLabel.setText("Abilities: " + unit.getAbilities());
-        }
+    //TODO: display unit data in labels
+    public void setUnit(Unit unit)
+    {
+        unitNameLabel.setText(unit.getUnitName());
+        unitTypeLabel.setText("Type: " + unit.getUnitType());
+        specializationLabel.setText("Spec: " + unit.getSpecialization());
+        statsLabel.setText("Price: " + unit.getPrice() + " | Armor: " + unit.getArmor() + " | Health: " + unit.getHealth());
+        abilitiesLabel.setText("Abilities: " + unit.getAbilities());
+    }
 
-        //Sets the Image on the Unit's card
+    //TODO: update image shown on card
     public void setImage(Image image)
     {
         unitImageView.setImage(image);
